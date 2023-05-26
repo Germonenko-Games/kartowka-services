109b4ebb67a9ea981d81b37d64df2c8e893af1d9
Slava Germonenko <slava.germonenko@gmail.com>
2023-05-26 23:00:36 +0200

    Setup migrations for Packs.

---
+++ src/Kartowka.Api/HostedServices/ApplyMigrationsHostedService.cs
@@ -0,0 +1,42 @@
+﻿using Kartowka.Core;
+using Microsoft.EntityFrameworkCore;
+
+namespace Kartowka.Api.HostedServices;
+
+public class ApplyMigrationsHostedService : BackgroundService
+{
+    private readonly IServiceProvider _serviceProvider;
+
+    private readonly ILogger<ApplyMigrationsHostedService> _logger;
+
+    public ApplyMigrationsHostedService(
+        IServiceProvider serviceProvider,
+        ILogger<ApplyMigrationsHostedService> logger
+    )
+    {
+        _serviceProvider = serviceProvider;
+        _logger = logger;
+    }
+
+    protected override async Task ExecuteAsync(CancellationToken stoppingToken)
+    {
+        var scope = _serviceProvider.CreateScope();
+
+        try
+        {
+            var context = scope.ServiceProvider.GetRequiredService<CoreContext>();
+
+            _logger.LogInformation("Started applying migrations");
+            await context.Database.MigrateAsync(stoppingToken);
+            _logger.LogInformation("Sucusefully applied migrations");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogCritical(ex, "Failed to apply migrations");
+        }
+        finally
+        {
+            scope.Dispose();
+        }
+    }
+}
--- src/Kartowka.Api/Program.cs
+++ src/Kartowka.Api/Program.cs
@@ -1,8 +1,26 @@
+using Kartowka.Api.HostedServices;
+using Kartowka.Core;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<CoreContext>(options =>
+{
+    var connectionString = builder.Configuration.GetConnectionString("Core");
+    options
+        .UseNpgsql(connectionString, postgresOptions =>
+        {
+            postgresOptions.MigrationsAssembly("Kartowka.Migrations");
+        })
+        .EnableSensitiveDataLogging(builder.Environment.IsDevelopment())
+        .UseSnakeCaseNamingConvention();
+});
+
+builder.Services.AddHostedService<ApplyMigrationsHostedService>();
 
 var app = builder.Build();
 
